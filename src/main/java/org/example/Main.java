d30de24faf006acbba1e3f00c4db320e6488f0bb
Danielberg86 <dunderberg86@hotmail.com>
2025-01-10 10:26:12 +0100

    Error created in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Github actions!");
-        System.out.println("Im a slow learner");
+        System.out.println("Im a slow learn
     }
 }
