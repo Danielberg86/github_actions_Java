13fe8e0a5516a05d07175e483ee162880843202b
Danielberg86 <dunderberg86@hotmail.com>
2025-01-10 10:22:44 +0100

    Added message in main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Github actions!");
+        System.out.println("Im a slow learner");
     }
 }
